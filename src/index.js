2afefbb92b274616ac2b222933a419112e7fa7d7
achen9149 <aaronchen9149@gmail.com>
2023-12-01 00:07:59 -0500

    basic function of music bot works, going to add show playlist/queue, delete from playlist functionality, and ability to play spotify playlist

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import discord from 'discord.js'
 import dotenv from 'dotenv'
 import { REST } from '@discordjs/rest'
-import { DisTube } from 'distube';
+import { DisTube, Queue } from 'distube';
 import { joinVoiceChannel } from '@discordjs/voice';
 
 dotenv.config();
@@ -31,18 +31,29 @@ client.on('messageCreate',async (message) => {
   if (message.author.bot) return;
   let prefix = "#"
   if (message.content[0] === prefix) {
-    console.log("within #")
     const messageArr = message.content.split(":");
     const cmd = messageArr[0];
     const song = messageArr[1];
-    if (cmd === "#play") {
+    if (cmd === ("#play")) {
+
       client.Distube.play(message.member.voice.channel, song, {
         member: message.member,
         textChannel: message.channel,
         message
       })
+    } else if (cmd === ("#pause")) {
+      client.Distube.pause(message)
+    } else if (cmd === ("#resume")) {
+      client.Distube.resume(message)
+    } else if (cmd === "#skip") {
+      client.Distube.skip(message)
+    } else if (cmd === "#shuffle") {
+      client.Distube.shuffle(message)
     }
   }
-  
+})
+client.Distube.on("playSong", (queue, song) => {
+  console.log(song.name)
+  queue.textChannel.send(`currently playing ${song.name}`)
 })
 client.login(process.env.TOKEN)
